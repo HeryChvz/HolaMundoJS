5c8c20e2966dffb2bb0955302c0e5af3fcff4a53
Hery <reyheri2010@hotmail.com>
2021-11-08 21:29:28 -0700

    Primer commit

---
+++ holamundo.js
@@ -0,0 +1,15 @@
+"use strict";
+
+console.log(
+  "Hola mundo desde Node.js, esto se vera en la terminal de comandos"
+);
+
+console.log(2 * 5);
+
+// console.log(window);
+
+console.log(global);
+
+setInterval(function () {
+  console.log("hola nodejs");
+}, 1000);
